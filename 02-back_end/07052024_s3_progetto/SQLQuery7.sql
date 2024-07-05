9f06dba41cdab6bbec7d7d9e5664a5b4f3a338e6
bertojr <1020bertone@gmail.com>
2024-07-05 14:21:24 +0200

    .

--- 02-back_end/07052024_s3_progetto/SQLQuery7.sql
+++ 02-back_end/07052024_s3_progetto/SQLQuery7.sql
@@ -1,1 +1,4 @@
 ﻿-- 7) Totale degli importi per ogni anagrafico --
+SELECT a.Nome, a.Cognome, SUM(v.Importo) as [Totale importi multe]
+FROM Anagrafica as a LEFT JOIN Verbale as v ON a.IdAnagrafica = v.FK_Anagrafica
+GROUP BY a.Nome, a.Cognome
--- 02-back_end/07052024_s3_progetto/SQLQuery8.sql
+++ 02-back_end/07052024_s3_progetto/SQLQuery8.sql
@@ -1,1 +1,4 @@
 ﻿-- 8) Visualizzazione di tutti gli anagrafici residenti a Palermo --
+SELECT *
+FROM Anagrafica as a
+WHERE a.Citta LIKE 'palermo'
