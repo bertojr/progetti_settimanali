0934ce11a6006ba64c9e54884771fa1086728982
bertojr <1020bertone@gmail.com>
2024-07-29 15:52:53 +0200

    .

--- 02-back_end/08022024_s6_progetto/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _08022024_s6_progetto.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
