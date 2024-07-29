b940a065d4f74da068e3f621c40788ccd5c3f42f
bertojr <1020bertone@gmail.com>
2024-07-29 19:52:36 +0200

    first commit

---
+++ 02-back_end/08022024_s7_progetto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _08022024_s7_progetto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
